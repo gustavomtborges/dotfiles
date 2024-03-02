514a9f60343be1613ee22f448a7d46cacf37a5cb
G M T Borges <gustavo@gmtborges.com>
2024-03-02 11:11:59 -0300

    update dotfiles

--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -104,17 +104,35 @@ return {
 		dependencies = "nvim-tree/nvim-web-devicons",
 		config = true,
 	},
-	-- {
-	-- 	"simrat39/symbols-outline.nvim",
-	-- 	config = function()
-	-- 		require("symbols-outline").setup({
-	-- 			position = "left",
-	-- 			width = 15,
-	-- 		})
-	--
-	-- 		vim.keymap.set("n", "<leader>to", ":SymbolsOutline<CR>")
-	-- 	end,
-	-- },
+	{
+		"stevearc/aerial.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("aerial").setup({
+				backends = { "lsp" },
+				filter_kind = { "Function", "Constant" },
+				layout = {
+					default_direction = "prefer_left",
+					min_width = 30,
+				},
+				open_automatic = false,
+				highlight_on_jump = false,
+				-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+				on_attach = function(bufnr)
+					-- Jump forwards/backwards with '{' and '}'
+					vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+					vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+				end,
+			})
+			-- You probably also want to set a keymap to toggle aerial
+			vim.keymap.set("n", "<leader>s", "<cmd>AerialToggle!<CR>")
+		end,
+	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
